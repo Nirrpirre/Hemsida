c299f36ee1f034162627c899cc086d12b98c8fe7
Noel <noel.forsstromeriksson@skola.taby.se>
2024-04-19 13:49:02 +0200

    hello

--- Hemsida/script.js
+++ Hemsida/script.js
@@ -1,15 +1,15 @@
-async function news(newsString) {
+async function news(searchQuery) {
     let apiKey = "25061337eed148e7bd9aed9c34ec64a1"
-    var url = `https://newsapi.org/v2/everything?q=technology&sources=${newsString}&apiKey=${apiKey}`;
+    var url = `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${apiKey}`;
     let response = await fetch(url)
     let json = await response.json()
     return json
 }
 
-
 function displayNews(data) {
     console.log(data);
     let resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = '';
 
     let articles = data.articles;
 
@@ -17,7 +17,7 @@ function displayNews(data) {
         let node = document.createElement("div");
         node.innerHTML = `
         <p>Author:      ${article.author}</p>
-        <p>Content: ${article.content}</p>
+        <p>description: ${article.description}</p>
         <p>Published:   ${article.publishedAt}</p>`;
         resultDiv.appendChild(node);
         let img = document.createElement("img")
@@ -27,8 +27,18 @@ function displayNews(data) {
 }
 
 async function getAndDisplay() {
-    const data = await news("CNN");
+    const searchQuery = document.getElementById("textbar").value;
+    const data = await news(searchQuery);
     displayNews(data);
 }
 
-getAndDisplay();
+document.getElementById("textbar").addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        getAndDisplay();
+    }
+});
+
+
+document.getElementById("Home").addEventListener("click", ()=>{
+    window.location.href="index.html"
+})
